2c83b13786f70cb60f04dc3ed4df4cadaa5d2fa1
Paul McGeachie <paulmcgeachie@icloud.com>
2022-03-30 07:00:25 +0100

    Addtop and bottom boundary

--- ball.js
+++ ball.js
@@ -1,3 +1,5 @@
+const INITIAL_VELOCITY = 0.025;
+
 export default class Ball {
   constructor(ballElem) {
     this.ballElem = ballElem;
@@ -20,21 +22,33 @@ set y(value) {
   this.ballElem.style.setProperty("--y",value);
 }
 
+rect() {
+  return this.ballElem.getBoundingClientRect();
+}
+
 reset() {
   this.x = 50;
-  this.y =50;
+  this.y = 50;
   this.direction = { x: 0 };
   while (Math.abs(this.direction.x) <= 0.2 || Math.abs(this.direction.x) >= 0.9) {
     const heading = randomNumberBetween(0, 2 * Math.PI);
     this.direction = {x: Math.cos(heading), y: Math.sin(heading)};
   }
-  console.log(this.direction);
+  this.velocity = INITIAL_VELOCITY;
 }
 
   update(delta){
-    this.x = 5;
-    this.y = 15;
+    this.x += this.direction.x * this.velocity * delta;
+    
+    this.y += this.direction.y * this.velocity * delta;
+
+    const rect = this.rect();
+
+    if (rect.bottom >= window.innerHeight || rect.top <= 0) {
+      this.direction.y *= -1;
+    }
   }
+
 }
 
 function randomNumberBetween(min,max) {
